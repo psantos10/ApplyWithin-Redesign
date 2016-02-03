112a12b7b3d071cafc130eb8154d94b11ee026b1
colin marshall <colinbfmarshall@gmail.com>
2016-02-03 21:52:56 +0000

    still got the same problem again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,7 @@ Rails.application.configure do
       user_name:            ENV["EMAIL"],
       password:             ENV["PASSWORD"],
       authentication:       'plain',
+      enable_starttls_auto: true,
     }
 
   config.action_mailer.perform_deliveries = true
