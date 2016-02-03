cac5959316a8d9a54fe66dba5d8649931277d552
colin marshall <colinbfmarshall@gmail.com>
2016-02-03 17:00:39 +0000

    small change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'applywithin.co.uk' }
+  config.action_mailer.default_url_options = { :host => 'quiet-wave-12643.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
